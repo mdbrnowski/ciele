896c3873a39f8fceb066419d8aede8aba8550bdc
Michał Dobranowski <mdbrnowski@gmail.com>
2026-01-17 13:27:49 +0100

    Initial commit

---
+++ src/ciele_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc ciele public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(ciele_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    ciele_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
